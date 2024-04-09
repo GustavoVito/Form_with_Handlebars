f31c4879f4c08d37a23f003a5fd7a3b3d9da8abc
GustavoVito <gustavocarmovito@gmail.com>
2024-04-09 13:19:49 -0300

    edit and delete function

--- app.js
+++ app.js
@@ -5,7 +5,18 @@ const handlebars = require('express-handlebars').engine
 const bodyParser = require('body-parser')
 const post = require ('./models/post')
 
-app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+app.engine(
+    "handlebars",
+    handlebars({
+      defaultLayout: "main",
+      helpers: {
+        isEqual: function (expectedValue, value) {
+          return value === expectedValue;
+        },
+      },
+    })
+  );
+
 app.set('view engine', 'handlebars')
 
 app.use(bodyParser.urlencoded({extended: false}))
@@ -40,6 +51,61 @@ app.get('/lista', function(req, res){
     })
     
 })
+
+app.get('/editar/:id', function(req, res){
+    post.findAll({where: {'id': req.params.id}}).then(function(post){
+        res.render('editar', {post: post})
+    }).catch(function(erro){
+        console.log('Erro: Agentamento não encontrado!' + erro)
+    })
+})
+
+app.post("/editar", function (req, res) {
+    post
+      .update(
+        {
+          nome: req.body.nome,
+          telefone: req.body.telefone,
+          origem: req.body.origem,
+          data_contato: req.body.data_contato,
+          observacao: req.body.observacao,
+        },
+        { where: { id: req.body.id } }
+      )
+      .then(function () {
+        console.log("Agendamento atualizado com sucesso!");
+      })
+      .catch(function (erro) {
+        console.log("Erro: Agendamento não atualizado!" + erro);
+      });
+    post
+      .findAll()
+      .then(function (post) {
+        res.render("lista", { post: post });
+      })
+      .catch(function (erro) {
+        console.log("Erro: Nenhum agendamento encontrado" + erro);
+      });
+  });
+
+app.get("/excluir/:id", function (req, res) {
+    post
+      .destroy({ where: { id: req.params.id } })
+      .then(function () {
+        post
+          .findAll()
+          .then(function (post) {
+            res.render("lista", { post: post });
+          })
+          .catch(function (erro) {
+            console.log("Erro: Nenhum agendamento encontrado", erro);
+          });
+        console.log("Agendamento excluido com sucesso!");
+      })
+      .catch(function (erro) {
+        console.log("Erro: Agendamento não excluido!" + erro);
+      });
+    })
 
 app.listen(8081, function(){
     console.log("Servidor Ativo!")
